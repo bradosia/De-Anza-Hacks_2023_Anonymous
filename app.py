907429bec7530bc18f969cdfb24fdda5e67c8d58
Brad <32529373+bradosia@users.noreply.github.com>
2023-10-20 18:33:47 -0700

    environment file

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import flask
+import os
 from flask import request, url_for, render_template, redirect
-
 
 app = flask.Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def my_maps():
 
-  mapbox_access_token = 'YOUR ACCESS TOKEN HERE'
+  mapbox_access_token = os.environ.get("MAPBOX_ACCESS_TOKEN")
 
   return render_template('index.html',
         mapbox_access_token=mapbox_access_token)
